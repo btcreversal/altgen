926ca2043c94cdf6ccba0589c5d93236de97929b
Lukas N <niedoba.lukas@gmail.com>
2018-02-25 15:02:50 +0100

    Updated instalpool.sh

--- installpool.sh
+++ installpool.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-DBUSER="dbuser"
+# change this to your coin reward address (where percentual mined reward goes)
+REWARDADDRESS="EYt8EWNW8ZbYujtVHdExKAFtowtqokohFi"
+
+DBUSER="root"
 DBPASS="dbpass"
 DBNAME="coiniumdb"
 
-RPCUSER="GERyFX9Fu3IPiE7a"
-RPCPASS="2LMNh4PKq2aVT39NdtwGDIQr9QCC4cJOnCzfVbEthwc3FLn6TSYqhT4jpbRbKMpU"
+RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1);
+RPCPASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1);
 RPCPORT="9999"
+
+elicoin-cli stop
+echo "" > /root/.elicoin/elicoin.conf
+echo -e rpcuser=$RPCUSER"\n"rpcpassword=$RPCPASS"\n"rpcport=$RPCPORT > /root/.elicoin/elicoin.conf
+elicoind -daemon
+sleep 10
 
 
 # find out what the magic bytes are
@@ -19,6 +28,9 @@ TESTMAGIC=`head -c 4 ~/.elicoin/testnet4/blocks/blk00000.dat |hexdump -e '16/1 "
 elicoin-cli stop
 elicoind -daemon
 sleep 5
+
+POOLADDRESS=`elicoin-cli getnewaddress`
+
 
 # install mono framework (needed to build and run C#)
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
@@ -72,23 +84,19 @@ cp src/CoiniumServ/Algorithms/Implementations/libyescrypt.so build/bin/Release
 
 
 
+sed -i "s|fbc0b6db|$MAINMAGIC|g" build/bin/Release/config/coins/elicoin.json
+sed -i "s|fcc1b7dc|$TESTMAGIC|g" build/bin/Release/config/coins/elicoin.json
 
-#
-# sed -i "s|127.0.0.1:3001|127.0.0.1:80|g" settings.json
-# sed -i "s|3001|80|g" settings.json
-# sed -i "s|Darkcoin|Elicoin|g" settings.json
-# sed -i "s|DRK|ELI|g" settings.json
-# sed -i "s|1337|0|g" settings.json
-# sed -i "s|1733320247b15ca2262be646397d1ffd6be953fa638ebb8f5dcbb4c2b91b34f1|$GENESIS_BLOCK|g" settings.json
-# sed -i "s|f270cd3813254c9922a2e222a56ba745842d9112223a1394062e460b33d27b7e|$GENESIS_TX|g" settings.json
-# sed -i "s|b2926a56ca64e0cd2430347e383f63ad7092f406088b9b86d6d68c2a34baef51|$GENESIS_BLOCK|g" settings.json
-# sed -i "s|65f705d2f385dc85763a317b3ec000063003d6b039546af5d8195a5ec27ae410|$GENESIS_TX|g" settings.json
-# sed -i "s|RBiXWscC63Jdn1GfDtRj8hgv4Q6Zppvpwb|$ADDRESS|g" settings.json
-#
-# sed -i "s|iquidus|$DBUSER|g" settings.json
-# sed -i "s|3xp!0reR|$DBPASS|g" settings.json
-# sed -i "s|explorerdb|$DBNAME|g" settings.json
-#
-# sed -i "s|darkcoinrpc|$RPCUSER|g" settings.json
-# sed -i "s|123gfjk3R3pCCVjHtbRde2s5kzdf233sa|$RPCPASS|g" settings.json
-# sed -i "s|9332|$RPCPORT|g" settings.json
+sed -i "s|dbusername|$DBUSER|g" build/bin/Release/config/pools/default.json
+sed -i "s|dbpassword|$DBPASS|g" build/bin/Release/config/pools/default.json
+
+sed -i "s|9333|$RPCPORT|g" build/bin/Release/config/pools/pool.json
+sed -i "s|rpcuser|$RPCUSER|g" build/bin/Release/config/pools/pool.json
+sed -i "s|rpcpass|$RPCPASS|g" build/bin/Release/config/pools/pool.json
+sed -i "s|false|true|g" build/bin/Release/config/pools/pool.json
+sed -i "s|n3Mvrshbf4fMoHzWZkDVbhhx4BLZCcU9oY|$POOLADDRESS|g" build/bin/Release/config/pools/pool.json
+sed -i "s|myxWybbhUkGzGF7yaf2QVNx3hh3HWTya5t|$REWARDADDRESS|g" build/bin/Release/config/pools/pool.json
+
+sed -i "s|dbname|$DBNAME|g" build/bin/Release/config/pools/default.json
+sed -i "s|dbusername|$DBUSER|g" build/bin/Release/config/pools/default.json
+sed -i "s|dbpassword|$DBPASS|g" build/bin/Release/config/pools/default.json
